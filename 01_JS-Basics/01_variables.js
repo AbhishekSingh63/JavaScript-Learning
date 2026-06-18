b55fcb82954d84be5be9e2cc8ee75c725df9f767
Abhishek Singh <150329098+AbhishekSingh63@users.noreply.github.com>
2026-06-18 08:05:28 +0000

    variables basics

--- 01_JS-Basics/01_variables.js
+++ 01_JS-Basics/01_variables.js
@@ -1,1 +1,17 @@
 const accountId=144553
+let accountEmail="abhi123@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState
+
+// accountId=2 // re-assignment not allowed
+
+accountEmail="abcd.com"
+accountPassword="21212121"
+accountCity="Bengaluru"
+
+console.log(accountId)
+
+// Prefer not to use var, because of issue in block scope and functional scope
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]) // to print multiple variables at once
